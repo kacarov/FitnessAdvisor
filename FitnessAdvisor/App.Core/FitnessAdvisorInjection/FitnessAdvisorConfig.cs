140a2d7eeb3976a6e74eb75d2bb2bc00c9666e84
Kacarov <kacarov.martin@gmail.com>
2018-08-26 21:38:03 +0300

    Add AutoFac and Moq, Skeleton for Autofac

---
+++ FitnessAdvisor/App.Core/FitnessAdvisorInjection/FitnessAdvisorConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Core.FitnessAdvisorInjection
+{
+    public class FitnessAdvisorConfig
+    {
+        IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            RegisterCoreComponents(builder);
+
+            var container = builder.Build();
+
+            return container;
+        }
+
+        public void RegisterCoreComponents(ContainerBuilder builder)
+        {
+            //Register core components Bai Ivane
+        }
+    }
+}
---
+++ FitnessAdvisor/FitnessAdvisor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FitnessAdvisor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
