36d033f82082f2554ac42894b0131a8a6f0e7873
Kacarov <kacarov.martin@gmail.com>
2018-08-28 01:15:40 +0300

    Added factory

---
+++ FitnessAdvisor/App.Core/Factory/TransformationGoalFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Models.Contracts;
+using App.Models.GeneralPurpose;
+
+namespace App.Core.Factory
+{
+    public class TransformationGoalFactory : ITransformationGoalFactory
+    {
+        public IBodyTransformationGoal GetGoal(int selectedRadioGroup, double weight, double currentFatPerc, int caloriesNeed)
+        {
+            switch (selectedRadioGroup)
+            {
+                case 0:
+                    return new Bulk(weight, currentFatPerc, caloriesNeed);
+                    
+                case 1:
+                    return new Maintain(weight, currentFatPerc, caloriesNeed);
+
+                case 2:
+                    return new Cutting(weight, currentFatPerc, caloriesNeed);
+
+            }
+            return null;
+        }
+    }
+}
